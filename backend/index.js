e2e4e10bfba77d7833c33962668d1e42d509b377
vinylen2 <gabrielwallen93@gmail.com>
2018-01-04 22:29:11 +0100

    model work

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const Koa = require('koa');
+const router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const config = require('./config.json');
+const cors = require('koa2-cors');
+const models = require('./models');
+const session = require('koa-session2');
+
+const app = new Koa();
+require('koa-qs')(app, 'strict');
+
+app.keys = ['secret', config.secret];
+app.proxy = true;
+
+// Set up body parsing middleware
+app.use(bodyParser());
+app.use(session());
+
+
+// Enable CORS
+app.use(cors({
+  origin: 'http://localhost:8080',
+  credentials: true,
+  allowMethods: ['GET', 'PATCH', 'POST'],
+  allowHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+}));
+
+// Require the Router defined in words.js
+// const assignment = require('./routes/assignment.js');
+
+app.listen(config.port);
+
+models.connection.sync({alter: true}).then(() => {
+  console.log(`Server listening on port: ${config.port}`);
+  console.log('Sequelize synchronized');
+//   app.use(assignment.routes());
+});
