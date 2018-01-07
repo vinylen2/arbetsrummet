f55fe3098cd5a13b4a68670bbfc1b92ed35e93a3
vinylen2 <gabrielwallen93@gmail.com>
2018-01-07 20:06:59 +0100

    added a toolbar

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <navbar></navbar>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar';
 export default {
+  components: {
+    Navbar,
+  },
   name: 'app'
 }
 </script>
@@ -18,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ vue-frontend/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="navbar">
+    <md-toolbar class="md-accent">
+      <div class="md-toolbar-row">
+        <div class="md-toolbar-section-start">
+          <md-button class="md-icon-button">
+            <md-icon>menu</md-icon>
+          </md-button>
+          <h3 class="md-title">Title on a second row</h3>
+        </div>
+        <div class="md-toolbar-section-end">
+          <md-button class="md-icon-button">
+            <md-icon>refresh</md-icon>
+          </md-button>
+          <md-button class="md-icon-button">
+            <md-icon>more_vert</md-icon>
+          </md-button>
+        </div>
+      </div>
+    </md-toolbar>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial);
+
+export default {
+  name: 'navbar',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
